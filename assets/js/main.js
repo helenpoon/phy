286d40560caa49d6583c5005a62a32f3389b89e3
Stephanie Wong <steph.wong.97@gmail.com>
2019-01-01 19:55:46 +0800

    fixed responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,58 @@
 $(document).ready(function() {
+	animate();
+});
 
+
+function animate() {
+	if ($(window).width() < 665) {
+		$(".column").css("width", "100%");
+	}
 // jQuery Function Number 1
-	$(".img").mouseover(function() {
+	$(".column").mouseover(function() {
+		if ($(window).width() < 665) {
+			$(".column").css("width", "100%");
+			return;
+		}
 		if (this.id == "1") {
 			// jQuery Function Number 2
-			$(this).css("width", "45%").css("height", "420px");
-			$("#2, #3, #4").css("width", "13%").css("height", "420px");
+			$(this).css("width", "45%").css("height", "100%");
+			$("#2, #3, #4").css("width", "18%").css("height", "100%");
 		} else if (this.id == "2") {
-			$(this).css("width", "45%").css("height", "420px");
-			$("#1, #3, #4").css("width", "13%").css("height", "420px");
+			$(this).css("width", "45%").css("height", "100%");
+			$("#1, #3, #4").css("width", "18%").css("height", "100%");
 		} else if (this.id == "3") {
-			$(this).css("width", "45%").css("height", "420px");
-			$("#1, #2, #4").css("width", "13%").css("height", "420px");
+			$(this).css("width", "45%").css("height", "100%");
+			$("#1, #2, #4").css("width", "18%").css("height", "100%");
 		} else {
-			$(this).css("width", "45%").css("height", "420px");
-			$("#1, #2, #3").css("width", "13%").css("height", "420px");
+			$(this).css("width", "45%").css("height", "100%");
+			$("#1, #2, #3").css("width", "18%").css("height", "100%");
 		}
-	})
+	});
 
 // jQuery Function Number 3
-	$(".img").mouseout(function() {
-		if (this.id == "1") {
-			$(this).css("width", "250px").css("height", "420px");
-			$("#2, #3, #4").css("width", "250px").css("height", "420px");
-		} else if (this.id == "2") {
-			$(this).css("width", "250px").css("height", "420px");
-			$("#1, #3, #4").css("width", "250px").css("height", "420px");
-		} else if (this.id == "3") {
-			$(this).css("width", "250px").css("height", "420px");
-			$("#1, #2, #4").css("width", "250px").css("height", "420px");
-		} else {
-			$(this).css("width", "250px").css("height", "420px");
-			$("#1, #2, #3").css("width", "250px").css("height", "420px");
+	$(".column").mouseout(function() {
+		if ($(window).width() < 665) {
+			$(".column").css("width", "100%");
+			return;
 		}
-	})
-
-	
-
+		if (this.id == "1") {
+			$(this).css("width", "25%").css("height", "100%");
+			$("#2, #3, #4").css("width", "25%").css("height", "100%");
+		} else if (this.id == "2") {
+			$(this).css("width", "25%").css("height", "100%");
+			$("#1, #3, #4").css("width", "25%").css("height", "100%");
+		} else if (this.id == "3") {
+			$(this).css("width", "25%").css("height", "100%");
+			$("#1, #2, #4").css("width", "25%").css("height", "100%");
+		} else {
+			$(this).css("width", "25%").css("height", "100%");
+			$("#1, #2, #3").css("width", "25%").css("height", "100%");
+		}
 	});
+	
+}
+
+$(window).resize(function() {
+	animate();
+});
 
